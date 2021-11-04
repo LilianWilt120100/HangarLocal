4dc3ac2c32a8a9a0f4a4c5f6b7671426ef334c0d
Wilt Lilian <lilian.wilt@hotmail.com>
2021-11-04 16:19:27 +0100

    add some routes in index

--- Hangar_App/public/index.php
+++ Hangar_App/public/index.php
@@ -4,6 +4,8 @@ session_start();
 
 use Slim\Factory\AppFactory;
 use App\Models\Producteur;
+use App\Models\Commande;
+use App\Models\Produit;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Illuminate\Database\Capsule\Manager as Capsule;
@@ -28,11 +30,54 @@ $app->get('/', function (Request $request, Response $responce, $parameters) {
 
 $app->get('/producteur', function (Request $request, Response $responce, $parameters) {
     $pr = new Producteur();
-    $pr = $pr->findById(1);
+    $pr = $pr->all();
     $responce->getBody()->write(''.$pr);
-    return $responce
+    return $responce;
         
-;
 });
+
+$app->get('/producteur/{id}', function (Request $request, Response $responce, $parameters) {
+    $pr = new Producteur();
+    $id = $parameters['id'];
+    $pr = $pr->findById($id);
+    $responce->getBody()->write(''.$pr);
+    return $responce;
+        
+});
+
+$app->get('/commande', function (Request $request, Response $responce, $parameters) {
+    $c = new Commande();
+    $c = $c->all();
+    $responce->getBody()->write(''.$c);
+    return $responce;
+        
+});
+
+$app->get('/commande/{id}', function (Request $request, Response $responce, $parameters) {
+    $c = new Commande();
+    $id = $parameters['id'];
+    $c = $c->findById($id);
+    $responce->getBody()->write(''.$c);
+    return $responce;
+        
+});
+
+$app->get('/produit', function (Request $request, Response $responce, $parameters) {
+    $p = new Produit();
+    $p = $p->all();
+    $responce->getBody()->write(''.$p);
+    return $responce;
+        
+});
+
+$app->get('/produit/{id}', function (Request $request, Response $responce, $parameters) {
+    $p = new Produit();
+    $id = $parameters['id'];
+    $p = $p->findById($id);
+    $responce->getBody()->write(''.$p);
+    return $responce;
+        
+});
+
 
 $app->run();
