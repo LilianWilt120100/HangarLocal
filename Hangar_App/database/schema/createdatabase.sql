1f31ac2ded9c8fca982d9e9dbe34998004e548b1
Jules <jfrancois968@gmail.com>
2021-11-05 16:16:32 +0100

    🗃️ dump

--- Hangar_App/database/schema/createdatabase.sql
+++ Hangar_App/database/schema/createdatabase.sql
@@ -1,222 +1,90 @@
--- phpMyAdmin SQL Dump
--- version 5.1.1
--- https://www.phpmyadmin.net/
---
--- Hôte : 127.0.0.1
--- Généré le : jeu. 04 nov. 2021 à 16:39
--- Version du serveur : 10.4.21-MariaDB
--- Version de PHP : 7.4.23
+-- Adminer 4.6.3 MySQL dump
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+SET NAMES utf8;
+SET time_zone = '+00:00';
+SET foreign_key_checks = 0;
+SET sql_mode = 'NO_AUTO_VALUE_ON_ZERO';
 
+DROP DATABASE IF EXISTS `hangarlocal`;
+CREATE DATABASE `hangarlocal` /*!40100 DEFAULT CHARACTER SET latin1 */;
+USE `hangarlocal`;
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
-
---
--- Base de données : `hangarlocal`
---
-
--- --------------------------------------------------------
-
---
--- Structure de la table `categorieproduit`
---
-
+DROP TABLE IF EXISTS `categorieproduit`;
 CREATE TABLE `categorieproduit` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(50) NOT NULL,
-  `description` varchar(200) NOT NULL
+  `description` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Déchargement des données de la table `categorieproduit`
---
-
 INSERT INTO `categorieproduit` (`id`, `nom`, `description`) VALUES
-(1, 'legumes', 'legumes');
-INSERT INTO `categorieproduit` (`nom`, `description`)
-VALUES ('Fruit', 'Toujours de saison');
+(1,	'legumes',	'legumes'),
+(2,	'Fruit',	'Toujours de saison');
 
--- --------------------------------------------------------
-
---
--- Structure de la table `commande`
---
-
+DROP TABLE IF EXISTS `commande`;
 CREATE TABLE `commande` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nom_client` varchar(50) NOT NULL,
   `mail_client` varchar(50) NOT NULL,
   `tel_client` varchar(12) NOT NULL,
   `montant` float NOT NULL,
   `etat` varchar(25) NOT NULL,
-  `lieu_retrait` varchar(200) NOT NULL
+  `lieu_retrait` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Déchargement des données de la table `commande`
---
-
 INSERT INTO `commande` (`id`, `nom_client`, `mail_client`, `tel_client`, `montant`, `etat`, `lieu_retrait`) VALUES
-(1, 'beugheu', 'beubeu@me.com', '0123456789', 34.5, 'en cours', 'Magasin Leuhclair\r\n');
+(1,	'beugheu',	'beubeu@me.com',	'0123456789',	34.5,	'en cours',	'Magasin Leuhclair\r\n'),
+(2,	'nom',	'maail@mail',	'123456',	22,	'en cours',	'ieu_retrait');
 
--- --------------------------------------------------------
-
---
--- Structure de la table `producteur`
---
-
+DROP TABLE IF EXISTS `producteur`;
 CREATE TABLE `producteur` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(50) NOT NULL,
   `localisation` varchar(100) NOT NULL,
   `mail` varchar(50) NOT NULL,
-  `urlImage` varchar(250) NULL;
+  `urlImage` varchar(250) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `login` (`nom`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Déchargement des données de la table `producteur`
---
+INSERT INTO `producteur` (`id`, `nom`, `localisation`, `mail`, `urlImage`) VALUES
+(1,	'Jean Michel deut',	'22 rue de la Gare',	'jm2@mail.fr',	'../assets/img/michel.png'),
+(2,	'abc',	'test',	'i@o.com',	'../assets/img/michel.png');
 
-INSERT INTO `producteur` (`id`, `nom`, `localisation`, `mail`) VALUES
-(1, 'Jean Michel deut', '22 rue de la Gare', 'jm2@mail.fr','../assets/img/michel.png'),
-(2, 'abc', 'test', 'i@o.com', '../assets/img/michel.png');
-
--- --------------------------------------------------------
-
---
--- Structure de la table `produit`
---
-
+DROP TABLE IF EXISTS `produit`;
 CREATE TABLE `produit` (
-  `id` int(11) NOT NULL,
+  `id` int(11) NOT NULL AUTO_INCREMENT,
   `nom` varchar(50) NOT NULL,
   `description` varchar(200) NOT NULL,
   `urlImage` varchar(250) DEFAULT NULL,
   `taille_lot` varchar(100) NOT NULL,
   `tarif_unitaire` double NOT NULL,
   `id_producteur` int(11) NOT NULL,
-  `id_categorie` int(11) NOT NULL
+  `id_categorie` int(11) NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `id_categorie` (`id_categorie`),
+  KEY `id_producteur` (`id_producteur`),
+  CONSTRAINT `produit_ibfk_1` FOREIGN KEY (`id_producteur`) REFERENCES `producteur` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+  CONSTRAINT `produit_ibfk_2` FOREIGN KEY (`id_categorie`) REFERENCES `categorieproduit` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Déchargement des données de la table `produit`
---
-
 INSERT INTO `produit` (`id`, `nom`, `description`, `urlImage`, `taille_lot`, `tarif_unitaire`, `id_producteur`, `id_categorie`) VALUES
-(1, 'carotte', 'grosse', 'http://localhost/phpmyadmin/themes/pmahomme/img/logo_left.png', '1 kg', 2.5, 1, 1);
-INSERT INTO `produit` (`nom`, `description`, `urlImage`, `taille_lot`, `tarif_unitaire`, `id_producteur`, `id_categorie`)
-VALUES ('Poivron à l unités', 'très frais', '../assets/img/poivron.png', '1', '4', '1', '1');
+(1,	'carotte',	'grosse',	'http://localhost/phpmyadmin/themes/pmahomme/img/logo_left.png',	'1 kg',	2.5,	1,	1),
+(2,	"Poivron à l\'unités",	'très frais',	'../assets/img/poivron.png',	'1',	4,	1,	1);
 
--- --------------------------------------------------------
-
---
--- Structure de la table `quantite`
---
-
+DROP TABLE IF EXISTS `quantite`;
 CREATE TABLE `quantite` (
   `id_commande` int(11) NOT NULL,
   `id_produit` int(11) NOT NULL,
-  `quantite` int(11) NOT NULL
+  `quantite` int(11) NOT NULL,
+  PRIMARY KEY (`id_commande`,`id_produit`),
+  KEY `id_produit` (`id_produit`),
+  CONSTRAINT `quantite_ibfk_1` FOREIGN KEY (`id_commande`) REFERENCES `commande` (`id`),
+  CONSTRAINT `quantite_ibfk_2` FOREIGN KEY (`id_produit`) REFERENCES `produit` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
---
--- Déchargement des données de la table `quantite`
---
-
 INSERT INTO `quantite` (`id_commande`, `id_produit`, `quantite`) VALUES
-(1, 1, 17);
+(1,	1,	17);
 
---
--- Index pour les tables déchargées
---
-
---
--- Index pour la table `categorieproduit`
---
-ALTER TABLE `categorieproduit`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `commande`
---
-ALTER TABLE `commande`
-  ADD PRIMARY KEY (`id`);
-
---
--- Index pour la table `producteur`
---
-ALTER TABLE `producteur`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE `login` (`nom`);
-
---
--- Index pour la table `produit`
---
-ALTER TABLE `produit`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `id_categorie` (`id_categorie`),
-  ADD KEY `id_producteur` (`id_producteur`);
-
---
--- Index pour la table `quantite`
---
-ALTER TABLE `quantite`
-  ADD PRIMARY KEY (`id_commande`,`id_produit`),
-  ADD KEY `id_produit` (`id_produit`);
-
---
--- AUTO_INCREMENT pour les tables déchargées
---
-
---
--- AUTO_INCREMENT pour la table `categorieproduit`
---
-ALTER TABLE `categorieproduit`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT pour la table `commande`
---
-ALTER TABLE `commande`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- AUTO_INCREMENT pour la table `producteur`
---
-ALTER TABLE `producteur`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=3;
-
---
--- AUTO_INCREMENT pour la table `produit`
---
-ALTER TABLE `produit`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
-
---
--- Contraintes pour les tables déchargées
---
-
---
--- Contraintes pour la table `produit`
---
-ALTER TABLE `produit`
-  ADD CONSTRAINT `produit_ibfk_1` FOREIGN KEY (`id_producteur`) REFERENCES `producteur` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
-  ADD CONSTRAINT `produit_ibfk_2` FOREIGN KEY (`id_categorie`) REFERENCES `categorieproduit` (`id`);
-
---
--- Contraintes pour la table `quantite`
---
-ALTER TABLE `quantite`
-  ADD CONSTRAINT `quantite_ibfk_1` FOREIGN KEY (`id_commande`) REFERENCES `commande` (`id`),
-  ADD CONSTRAINT `quantite_ibfk_2` FOREIGN KEY (`id_produit`) REFERENCES `produit` (`id`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- 2021-11-05 15:14:26
